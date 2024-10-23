5af44738081b0d3ea92e29ab1d0f3fd4cec77a07
Pramod <83743397+pramod-d04@users.noreply.github.com>
2024-10-23 16:13:00 +0530

    Time: 2 ms (98.78%), Space: 45.1 MB (18.90%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> m = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int comp = target - nums[i];
+            
+            if(m.containsKey(comp)){
+                return new int[]{m.get(comp), i};
+            }
+            
+            m.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No Solution");
+    }
+}
